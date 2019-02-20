4b2822d341fbf622ac6fbb2396b41210a5803320
Navdeep Singh <me@navdeepsingh.ca>
2019-02-19 16:52:05 -0800

    Add a unit test

---
+++ src/routes/Home/BaniList/BaniListReducer.spec.js
@@ -0,0 +1,31 @@
+import { banis } from './BaniListReducers';
+import { UPDATE_BANIS_LIST } from './BaniListActions';
+
+describe('sets the list of Banis', () => {
+  const stateBefore = [];
+  it('should return an empty array on initialization', () => {
+    const action = {
+      type: '',
+    };
+    expect(banis(undefined, action))
+      .toEqual(stateBefore);
+  });
+
+  it('should add Banis to an empty state', () => {
+    const action = {
+      type: UPDATE_BANIS_LIST,
+      payload: {
+        banis: [{
+          ID: 1,
+          gurmukhi: 'gur mMqR',
+          gurmukhiUni: 'ਗੁਰ ਮੰਤ੍ਰ',
+          token: 'gurmantar',
+          transliteration: 'gur ma(n)tr',
+          updated: '2017-09-23 03:28:45',
+        }],
+      },
+    };
+    expect(banis([], action))
+      .toEqual(action.payload.banis);
+  });
+});
