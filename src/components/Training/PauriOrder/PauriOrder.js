bd818354a8b67a8a3ed7c1b8edcdbc306541f6fc
Navdeep Singh <me@navdeepsingh.ca>
2019-02-27 20:12:25 -0800

    Pauri Order v1

--- src/components/Training/PauriOrder/PauriOrder.js
+++ src/components/Training/PauriOrder/PauriOrder.js
@@ -1,44 +1,82 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/styles';
 // Components
 import BaniTitle from '../../BaniTitle';
 // Utils
 import getSections from '../../../utils/getSections';
+import splitCorrect from '../../../utils/splitCorrect';
 
 // Actions
 import { fetchBaniAction } from '../../../actions/BaniActions';
 
-const PauriOrder = ({ banis, match, fetchBani }) => {
+const styles = {
+  option: {
+    cursor: 'pointer',
+  },
+  sectionPaper: {
+    padding: '10px',
+  },
+  section: {
+    'margin-bottom': '15px',
+  },
+};
+
+const PauriOrder = ({
+  banis,
+  classes,
+  fetchBani,
+  match,
+}) => {
   const baniToken = match.params.bani;
   const bani = banis.find(oneBani => oneBani.token === baniToken);
+  const initialState = 0;
+  const [correct, setCorrect] = useState(initialState);
   useEffect(() => {
     if (!bani.gurbani) {
       fetchBani(bani.ID);
     }
   });
   const sections = getSections(bani);
+  const { correctArray, nextID, nextOptions } = splitCorrect(sections, correct);
+
   return (
     <React.Fragment>
       <BaniTitle text={bani.gurmukhi} />
       <div className="gurmukhi">
-        {Object.keys(sections).map(sectionID => (
-          <div key={`section${sectionID}`} style={{ marginBottom: '15px' }}>
-            {sections[sectionID].map((verse, index) => (
-              <React.Fragment key={`line${index}`}>
-                {verse}
+        <Paper className={[classes.section, classes.sectionPaper].join(' ')}>
+          {correctArray.map(sectionID => (
+            <div key={`section${sectionID}`} className={classes.section}>
+              {sections[sectionID].map((verse, index) => (
+                <React.Fragment key={`line${index}`}>
+                  {verse}
+                  {' '}
+                </React.Fragment>
+              ))}
+            </div>
+          ))}
+        </Paper>
+        <Grid container spacing={24}>
+          {nextOptions.map(sectionID => (
+            <Grid key={`section${sectionID}`} item xs={12} sm={4}>
+              <Paper onClick={() => { if (sectionID === nextID) { setCorrect(nextID); } }} className={[classes.option, classes.sectionPaper].join(' ')}>
+                {sections[sectionID][0]}
                 {' '}
-              </React.Fragment>
-            ))}
-          </div>
-        ))}
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </React.Fragment>
   );
 };
 PauriOrder.propTypes = {
   banis: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  classes: PropTypes.shape({}).isRequired,
   fetchBani: PropTypes.func.isRequired,
   match: PropTypes.shape({}).isRequired,
 };
@@ -51,4 +89,4 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   fetchBani: fetchBaniAction,
 }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(PauriOrder);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(PauriOrder));
---
+++ src/utils/splitCorrect.js
@@ -0,0 +1,38 @@
+const shuffle = (a) => {
+  const b = [...a];
+  for (let i = b.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [b[i], b[j]] = [b[j], b[i]];
+  }
+  return b;
+};
+
+const splitCorrect = (sections, correct) => {
+  const sectionIDs = Object.keys(sections);
+  const remaining = sectionIDs.filter(sectionID => sectionID > correct);
+  let nextID;
+  const nextOptions = [];
+  if (remaining.length > 0) {
+    ([nextID] = remaining.splice(0, 1));
+    nextID = parseInt(nextID, 10);
+    nextOptions.push(nextID);
+  }
+  if (remaining.length > 0) {
+    const nextTwoIndex = Math.floor(Math.random() * remaining.length);
+    const [nextTwoID] = remaining.splice(nextTwoIndex, 1);
+    nextOptions.push(parseInt(nextTwoID, 10));
+  }
+  if (remaining.length > 0) {
+    const nextThreeIndex = Math.floor(Math.random() * remaining.length);
+    const [nextThreeID] = remaining.splice(nextThreeIndex, 1);
+    nextOptions.push(parseInt(nextThreeID, 10));
+  }
+  const shuffledNextOptions = shuffle(nextOptions);
+  return {
+    correctArray: sectionIDs.filter(sectionID => sectionID <= correct),
+    nextID,
+    nextOptions: shuffledNextOptions,
+  };
+};
+
+export default splitCorrect;
