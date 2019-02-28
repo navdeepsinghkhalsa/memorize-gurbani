552ac391895f744ca3f7c309d60df112d38aac58
Navdeep Singh <me@navdeepsingh.ca>
2019-02-27 20:17:08 -0800

    Add quick and dirty about text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,6 +5,8 @@ import TitleBar from '../TitleBar';
 const About = () => (
   <React.Fragment>
     <TitleBar text="About" />
+    {'This application is a work in progress. Follow progress on '}
+    <a href="https://github.com/navdeepsinghkhalsa/memorize-gurbani" target="_blank" rel="noopener noreferrer">GitHub</a>
   </React.Fragment>
 );
 
